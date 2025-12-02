9765b72ae46da607c863aa59e919480fdb24ba9c
xiaomian yang <xiaomian@mit.edu>
2025-12-02 16:49:00 -0500

    minor changes

--- gemm.cuh
+++ gemm.cuh
@@ -35,13 +35,58 @@ __device__ void gemm_naive_XY(
     }
 }
 
-__device__ void gemm_helper (gemm_struct &gemm_struct) {
-
-
-}
-
 __device__ void index (int32_t row_ID, int32_t col_ID, int32_t offset_col, int32_t offset_row, int32_t dim) {
     return (offset_col + row_ID) * dim + (offset_row + col_ID);
+}
+
+__device__ void block_gemm (Gemm_Struct gemm_info, float *shared_mem, int32_t tile_size) {
+
+    int32_t N = gemm_info.N;
+    int32_t block_n = gemm_info.block_n;
+    float const *B1 = gemm_info.B1;
+    float const *B2 = gemm_info.B2;
+    float *out = gemm_info.out;
+    int32_t B1_row_offset = gemm_info.B1_row_offset;
+    int32_t B1_col_offset = gemm_info.B1_col_offset;
+    int32_t B2_row_offset = gemm_info.B2_row_offset;
+    int32_t B2_col_offset = gemm_info.B2_col_offset;
+
+    
+
+    shared_mem[index(row_ID, col_ID, 0, 0, block_n)] = B1[index(row_ID, col_ID, B1_col_offset, B1_row_offset, N)];
+    shared_mem[index(row_ID, col_ID, 0, 0, block_n) + block_n * block_n] = B2[index(row_ID, col_ID, B2_col_offset, B2_row_offset, N)];
+    __syncthreads();
+
+    int32_t col_ID = threadIdx.x % block_n;
+    int32_t row_ID = threadIdx.x / block_n;
+
+    float sum[tile_size * tile_size];
+    float a_val[tile_size];
+    #pragma unroll
+    for (int32_t i = 0; i < tile_size * tile_size; ++i) {
+        sum[i] = 0.0f;
+    }
+    #pragma unroll
+    for (int32_t i = 0; i < tile_size; ++i) {
+        a_val[i] = 0.0f;
+    }
+
+    for (int32_t i = 0; i < block_n; ++i) {
+        // computing B1 * B2^T
+        for (int32_t tile_row = 0; tile_row < tile_size; ++tile_row) {
+            for (int32_t tile_col = 0; tile_col < tile_size; ++tile_col) {
+
+            }
+        }
+
+        for (int32_t tile_row = 0; tile_row < tile_size; ++tile_row) {
+            for (int32_t tile_col = 0; tile_col < tile_size; ++tile_col) {
+
+            }
+        }
+        sum += shared_mem[index(row_ID, i, 0, 0, block_n)] * shared_mem[index(col_ID, i, 0, 0, block_n) + block_n * block_n];
+    }
+    out[0] += sum;
 }
 
 struct Gemm_Struct {
@@ -59,9 +104,7 @@ struct Gemm_Struct {
 // Computes A-BB^T for one SM
 // N is now the total dimension of the matrix, block_n is the dimension of the block
 __device__ void gemm_one_SM(
-    Block_Update &block_info,
-    const uint32_t N, const uint32_t block_n, float const *A, float const *B, float *out,
-    uint32_t A_col_offset, uint32_t A_row_offset, uint32_t B_col_offset, uint32_t B_row_offset
+    Block_Update block_info, float *shared_mem
 ) {
 
     int32_t N = block_info.n;
@@ -77,34 +120,23 @@ __device__ void gemm_one_SM(
     gemm_struct.block_n = block_n;
     gemm_struct.out = out;
 
-    for (int32_t k = 0; k < j; ++k) {
-        gemm_struct.B1 = B + offset_i * block_n * N + k * block_n;
-        gemm_struct.B2 = B + k * block_n * N + offset_j * block_n;
+    out[0] = 0.0f;
+
+    for (int32_t k = 0; k < offset_j; ++k) {
+        gemm_struct.B1 = B;
+        gemm_struct.B2 = B;
         gemm_struct.B1_col_offset = offset_i * block_n;
         gemm_struct.B1_row_offset = k * block_n;
-        gemm_struct.B2_col_offset = k * block_n;
-        gemm_struct.B2_row_offset = offset_j * block_n;
-        gemm_helper(gemm_struct);
+        gemm_struct.B2_col_offset = offset_j * block_n;
+        gemm_struct.B2_row_offset = k * block_n;
+        gemm_helper(gemm_struct, shared_mem);
     }
 
     int32_t col_ID = threadIdx.x % block_n;
     int32_t row_ID = threadIdx.x / block_n;
     
     // subtract out from A
-    float sum = A[index(row_ID, col_ID, offset_i * block_n, offset_j * block_n, N)] = 
-
-
-    const uint32_t dim = N * block_n;
-    int32_t col_ID = threadIdx.x % block_n;
-    int32_t row_ID = threadIdx.x / block_n;
-
-    float sum = 0.0f;
-    for (uint32_t i = 0; i < block_n; ++i) {
-        sum += B[(B_col_offset + row_ID) * dim + (B_row_offset + i)] * B[(B_col_offset + col_ID) * dim + (B_row_offset + i)];
-    }
-
-    if (row_ID < block_n && col_ID < block_n) {
-        out[row_ID * block_n + col_ID] = A[(A_col_offset + row_ID) * dim + (A_row_offset + col_ID)] - sum;
-    }
+    // need to update with tile-size later-on!!!
+    out[0] = A[index(row_ID, col_ID, offset_i * block_n, offset_j * block_n, N)] - out[0];
 }
 } // namespace gemm_naive
