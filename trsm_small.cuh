fc5003878d8da876b80143b2173c3fd06922d459
Kameron Dawson <ksdawson@mit.edu>
2025-12-07 11:15:22 -0500

    add pragma unrolls to trsm its a lil faster. also working on new trsm that has more data reuse

--- trsm_small.cuh
+++ trsm_small.cuh
@@ -88,6 +88,7 @@ __device__ void block_forward_substitution(float const *A, float *x, float const
 ) {
   // Use local thread idx as this is done at the warp level
   const uint32_t thread_idx = threadIdx.x % 32;
+  #pragma unroll
   for (uint32_t i = 0; i < r; ++i) {
     // Each thread computes a piece of the sum
     float partial_sum = 0.0f;
@@ -119,28 +120,18 @@ template <bool x_row, bool b_row, uint32_t A_n, uint32_t x_n, uint32_t b_n, uint
 __device__ void block_forward_substitution(float const *A, float *x, float const *b) {
   // Use local thread idx as this is done at the warp level
   const uint32_t thread_idx = threadIdx.x % 32;
+  #pragma unroll
   for (uint32_t i = 0; i < r; ++i) {
     // Each thread computes a piece of the sum
     float partial_sum = 0.0f;
     for (uint32_t j = thread_idx; j < i; j += 32) {
-      if constexpr (x_row)
-        partial_sum += A[i * A_n + j] * x[j];
-      else
-        partial_sum += A[i * A_n + j] * x[j * x_n];
+      partial_sum += A[i * A_n + j] * x[j];
     }
     // Combine the sum across the warp
     float sum = utils::warp_prefix_sum<float>(partial_sum);
     // Last thread handles writing it back
     if (thread_idx == 31) {
-      float xi = 0.0f;
-      if constexpr (b_row)
-        xi = (b[i] - sum) / A[i * A_n + i];
-      else
-        xi = (b[i * b_n] - sum) / A[i * A_n + i];
-      if constexpr (x_row)
-        x[i] = xi;
-      else
-        x[i * x_n] = xi;
+      x[i] = (b[i] - sum) / A[i * A_n + i];
     }
     // All threads need this iteration to be done
     __syncwarp();
@@ -152,35 +143,52 @@ __device__ void block_forward_substitution(float const *A, float *x, float const
 ) {
   // Use local thread idx as this is done at the warp level
   const uint32_t thread_idx = threadIdx.x % 32;
+  #pragma unroll
   for (uint32_t i = 0; i < r; ++i) {
     // Each thread computes a piece of the sum
     float partial_sum = 0.0f;
     for (uint32_t j = thread_idx; j < i; j += 32) {
-      if constexpr (x_row)
-        partial_sum += A[i * A_n + j] * x[j];
-      else
-        partial_sum += A[i * A_n + j] * x[j * x_n];
+      partial_sum += A[i * A_n + j] * x[j];
     }
     // Combine the sum across the warp
     float sum = utils::warp_prefix_sum<float>(partial_sum);
     // Last thread handles writing it back
     if (thread_idx == 31) {
-      float xi = 0.0f;
-      if constexpr (b_row)
-        xi = (b[i] - sum) / A[i * A_n + i];
-      else
-        xi = (b[i * b_n] - sum) / A[i * A_n + i];
-      if constexpr (x_row)
-        x[i] = xi;
-      else
-        x[i * x_n] = xi;
+      x[i] = (b[i] - sum) / A[i * A_n + i];
     }
     // All threads need this iteration to be done
     __syncwarp();
   }
 }
 
-// no padding on A, X, B, ex. on shared memory
+// template <uint32_t W, uint32_t A_n, uint32_t X_n, uint32_t B_n, uint32_t r>
+// __device__ void new_block_trsm(float const *A, float *X, float const *B) {
+//   // Use local thread idx as this is done at the warp level
+//   const uint32_t thread_idx = threadIdx.x % 32;
+//   for (uint32_t i = 0; i < r; ++i) {
+//     // Each thread computes a piece of each sum
+//     float partial_sum[r/W] = {0.0f};
+//     for (uint32_t j = thread_idx; j < i; j += 32) {
+//       const float a = A[i * A_n + j];
+//       for (uint32_t idx = 0; idx < r/W; ++idx) {
+//         float *x = X[(idx * W) * X_n];
+//         partial_sum[idx] += a * x[j];
+//       }
+//     }
+//     // Combine the sums across the warp
+//     for (uint32_t idx = 0; idx < r/W; ++idx) {
+//       float sum = utils::warp_prefix_sum<float>(partial_sum[idx]);
+//       // Last thread handles writing it back
+//       if (thread_idx == 31) {
+//         float *x = X[(idx * W) * X_n];
+//         float *b = B[(idx * W) * B_n];
+//         x[i] = (b[i] - sum) / A[i * A_n + i];
+//       }
+//     }
+//     // All threads need this iteration to be done
+//     __syncwarp();
+//   }
+// }
 __device__ void block_trsm(float const *A, float *X, float const *B,
   const uint32_t A_n, const uint32_t X_n, const uint32_t B_n,
   const uint32_t r
